dff9c423da72e2460edc30ea66b6b4757924e4f6
Randy Burden <randy_burden@hotmail.com>
2012-04-28 19:40:53 -0500

    Added some auto-load assemblies logic

--- src/Scrumee.Web/Global.asax.cs
+++ src/Scrumee.Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -42,6 +43,8 @@ namespace Scrumee.Web
 
             RegisterRoutes( RouteTable.Routes );
 
+            LoadAllBinDirectoryAssemblies();
+
             ControllerBuilder.Current.SetControllerFactory( new Scrumee.Infrastructure.StructureMapControllerFactory() );
             
             Scrumee.Infrastructure.StructureMapBootstrapper.BootstrapStructureMap();
@@ -53,6 +56,24 @@ namespace Scrumee.Web
         protected void Application_EndRequest()
         {
             Scrumee.Infrastructure.StructureMapBootstrapper.ReleaseAndDisposeAllHttpScopedObjects();
+        }
+
+        public static void LoadAllBinDirectoryAssemblies()
+        {
+            string binPath = System.IO.Path.Combine( System.AppDomain.CurrentDomain.BaseDirectory, "bin" ); // note: don't use CurrentEntryAssembly or anything like that.
+
+            foreach ( string dll in Directory.GetFiles( binPath, "*.dll", SearchOption.AllDirectories ) )
+            {
+                try
+                {
+                    Assembly loadedAssembly = Assembly.LoadFile( dll );
+                }
+                catch ( FileLoadException loadEx )
+                { } // The Assembly has already been loaded.
+                catch ( BadImageFormatException imgEx )
+                { } // If a BadImageFormatException exception is thrown, the file is not an assembly.
+
+            } // foreach dll
         }
     }
 }
